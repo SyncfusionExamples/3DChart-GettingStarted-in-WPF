3bdc87401180a16b030d0b3ca3d939519c8f4701
ramyaprakash17 <65007314+ramyaprakash17@users.noreply.github.com>
2020-05-13 12:41:14 +0530

    Add files via upload
    Added sample with folder.

---
+++ 3DChart-GettingStarted/3DChart-GettingStarted/UserProfileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted_3DCharts
+{
+    public class UserProfile
+    {
+        public DateTime TimeStamp { get; set; }
+        public double NoOfUsers { get; set; }
+    }
+}
---
+++ 3DChart-GettingStarted/3DChart-GettingStarted/UserProfileViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted_3DCharts
+{
+    public class UsersViewModel
+    {
+        public UsersViewModel()
+        {
+            this.UsersList = new ObservableCollection<UserProfile>();
+
+            DateTime date = DateTime.Today;
+
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(0.5), NoOfUsers = 1000 });
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(1), NoOfUsers = 5000 });
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(1.5), NoOfUsers = 3000 });
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(2), NoOfUsers = 4000 });
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(2.5), NoOfUsers = 2000 });
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(3), NoOfUsers = 1000 });
+        }
+        public ObservableCollection<UserProfile> UsersList { get; set; }
+
+    }
+}
